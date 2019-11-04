10478e6ae91d7b3e149357508fb7035daf56af1a
Justyna Makowska <makowskafashion@gmail.com>
2019-11-04 01:44:33 +0100

    Create Redux Store

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import Navbar from './layout/Navbar';
 import ListUser from './pages/ListUser';
 import AddUser from './pages/AddUser';
@@ -11,13 +13,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/listUsers" component={ListUser}/>
-          <Route exact path="/addUser" component={AddUser}/>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/listUsers" component={ListUser} />
+            <Route exact path="/addUser" component={AddUser} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
